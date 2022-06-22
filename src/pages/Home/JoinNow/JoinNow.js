1fba924565a259782fa36ef77681d6e2897b7341
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-22 18:37:34 +0600

    new code added

--- src/pages/Home/JoinNow/JoinNow.js
+++ src/pages/Home/JoinNow/JoinNow.js
@@ -4,10 +4,10 @@ const JoinNow = () => {
   return (
     <div className="">
       <div className="text-center my-28">
-        <button className="mt-5 btn btn-secondary px-8 rounded-full shadow-sm text-xl mr-3 md:mr-10 ">
+        <button className="mt-5 btn btn-secondary px-5 md:px-8 rounded-full shadow-sm text-xl mr-3 md:mr-10 ">
           Join now
         </button>
-        <button className=" mt-5 btn btn-outline border-blue-600 border-4 text-primary px-10 rounded-full shadow-sm text-xl hover:bg-primary hover:text-white hover:border-primary duration-500 ">
+        <button className=" mt-5 btn btn-outline border-blue-600 border-4 text-primary px-7 md:px-10 rounded-full shadow-sm text-xl hover:bg-primary hover:text-white hover:border-primary duration-500 ">
           sign in
         </button>
       </div>
