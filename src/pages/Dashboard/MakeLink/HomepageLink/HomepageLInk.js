97b8cc7856cb9f6bb3b4576e317bc501982c1ec7
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-26 11:52:46 +0600

    new  code added

---
+++ src/pages/Dashboard/MakeLink/HomepageLink/HomepageLInk.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import daraz from "../../../../imgs/partners/daraz-logo.png";
+import bkash from "../../../../imgs/partners/bkash.png";
+import citybank from "../../../../imgs/partners/citybank.png";
+import nagad from "../../../../imgs/partners/nagad.svg";
+import walton from "../../../../imgs/partners/walton.png";
+import samsung from "../../../../imgs/partners/samsung.jpeg";
+
+export default class HomepageLInk extends Component {
+  constructor(props) {
+    super(props);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+  }
+  next() {
+    this.slider.slickNext();
+  }
+  previous() {
+    this.slider.slickPrev();
+  }
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+    };
+    return (
+      <div className="pb-10">
+        <div className="px-5 md:px-20 bg-gray-100 py-5 rounded-xl">
+          <Slider ref={(c) => (this.slider = c)} {...settings}>
+            <div key={1} className="mx-2 md:mx-10 pr-5 pb-2 ">
+              <div>
+                <div className="shadow-2xl bg-base-100 w-40 h-36 px-10 py-5 rounded-lg ">
+                  <img className="w-12 mx-auto" src={daraz} alt="" />
+                  <p className="mt-5 text-center">Daraz</p>
+                </div>
+              </div>
+            </div>
+            <div key={2} className="mx-2 md:mx-10 pr-5 pb-2">
+              <div>
+                <div className="shadow-2xl bg-base-100 w-40 h-36 px-10 py-6 rounded-lg">
+                  <img className="w-16 mx-auto" src={bkash} alt="" />
+                  <p className="mt-5  text-center">bKash</p>
+                </div>
+              </div>
+            </div>
+            <div key={3} className="mx-2 md:mx-10 pr-5 mr-5 pb-2">
+              <div>
+                <div className="shadow-2xl bg-base-100 w-40 h-36 px-10 py-4  rounded-lg">
+                  <img className="w-16" src={citybank} alt="" />
+                  <p className="mt-5 text-center">City Bank</p>
+                </div>
+              </div>
+            </div>
+            <div key={4} className="mx-2 md:mx-10 pr-5 mr-5 pb-2">
+              <div>
+                <div className="shadow-2xl bg-base-100 w-40 h-36 px-10 py-7  rounded-lg">
+                  <img className="w-28" src={nagad} alt="" />
+                  <p className="mt-4 text-center">Nagad</p>
+                </div>
+              </div>
+            </div>
+            <div key={5} className="mx-2 md:mx-10 pr-5 mr-5 pb-2">
+              <div>
+                <div className="shadow-2xl bg-base-100 w-40 h-36 px-10 py-3 rounded-lg">
+                  <img className="w-28 mx-auto" src={walton} alt="" />
+                  <p className="mt-3 text-center">Walton</p>
+                </div>
+              </div>
+            </div>
+            <div key={6} className="mx-2 md:mx-10 pr-5 mr-5 pb-2">
+              <div className="shadow-2xl bg-base-100 w-40 h-36 px-10 rounded-lg">
+                <img className="w-24 mx-auto" src={samsung} alt="" />
+                <p className="mt-5 text-center">Samsung</p>
+              </div>
+            </div>
+          </Slider>
+          <div className="mt-16" style={{ textAlign: "center" }}>
+            <button
+              className=" px-4 mr-3 py-2  bg-base-100 rounded-full shadow-md"
+              onClick={this.previous}
+            >
+              <FontAwesomeIcon icon={faAngleLeft} />
+            </button>
+            <button
+              className=" py-2 px-4 bg-base-100 rounded-full shadow-md"
+              onClick={this.next}
+            >
+              <FontAwesomeIcon icon={faAngleRight} />
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/pages/Dashboard/MakeLink/MakeLink.js
+++ src/pages/Dashboard/MakeLink/MakeLink.js
@@ -1,12 +1,89 @@
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import DashboardHeader from "../DashHeader/DashboardHeader";
 import Sidebar from "../Sidebar/Sidebar";
+import HomepageLink from "./HomepageLink/HomepageLInk";
+import HomepageLInkTwo from "./HomepageLink/HomepageLinkTwo";
 
 const MakeLink = () => {
   return (
     <div>
       <DashboardHeader></DashboardHeader>
-      <Sidebar></Sidebar>
+      <div>
+        <div class="drawer drawer-mobile bg-base-100">
+          <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+          <div class="drawer-content flex flex-col items-center  md:mt-20">
+            <label
+              for="my-drawer-2"
+              class="btn btn-primary drawer-button lg:hidden"
+            >
+              Open drawer
+            </label>
+
+            <div>
+              <div>
+                <h1 className="text-xl font-bold text-center">
+                  Make your own Profit Links in Seconds
+                </h1>
+                <div className="text-secondary bg-secondary w-48 mt-2 mx-auto">
+                  <hr className="w-20 text-secondary mx-auto" />
+                  <hr className="w-20 bg-secondary mx-auto" />
+                  <hr className="w-20 text-secondary bg-secondary mx-auto" />
+                </div>
+              </div>
+              <div className="py-10 bg-gray-100 mt-10 md:px-40 rounded-xl">
+                <p className="text-center">
+                  Paste a link from our active partner sites in the box below to
+                  make a link & share it.
+                </p>
+                <div className=" mx-auto justify-center py-10  flex">
+                  <div className="p-2 px-3.5 bg-primary rounded-full mr-[-2rem] z-20">
+                    <FontAwesomeIcon
+                      className="text-white"
+                      icon={faLink}
+                    ></FontAwesomeIcon>
+                  </div>
+                  <div className="z-10 shadow-lg rounded-r-full ">
+                    <input
+                      type="text"
+                      placeholder="Paste homepage or profile link here"
+                      class="input input-bordered w-full max-w-md pl-10 mr-28 focus:outline-none rounded-r-full"
+                    />
+                  </div>
+                </div>
+                <div className="text-center">
+                  <button className="btn px-7 text-white btn-primary rounded-full btn-sm">
+                    Make a Profile Link
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-[900px] h-72">
+              <div className="pb-10">
+                <h1 className="text-xl font-bold text-center mt-10">
+                  Quick Convert Homepage Links
+                </h1>
+                <div className="text-secondary bg-secondary w-48 mt-2 mx-auto">
+                  <hr className="w-20 text-secondary mx-auto" />
+                  <hr className="w-20 bg-secondary mx-auto" />
+                  <hr className="w-20 text-secondary bg-secondary mx-auto" />
+                </div>
+              </div>
+
+              <div className="hidden md:block">
+                <HomepageLink></HomepageLink>
+              </div>
+              <div className="block md:hidden mx-10">
+                <HomepageLInkTwo></HomepageLInkTwo>
+              </div>
+            </div>
+          </div>
+
+          <Sidebar></Sidebar>
+        </div>
+      </div>
     </div>
   );
 };
