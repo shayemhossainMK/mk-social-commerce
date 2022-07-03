2066bbc973809e03ddd8c1b09352095f900ddc73
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-07-03 10:10:53 +0600

    new odae

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 const useToken = (user) => {
   const [token, setToken] = useState("");
   useEffect(() => {
-    console.log("user", user);
     const email = user?.user?.email;
     const currentUser = { email: email };
     if (email) {
